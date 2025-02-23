c6015ded5708f73c872e7fbc34b6e8f2282651fb
young-mark <youngcmark@gmail.com>
2025-02-23 17:41:40 +0900

    ページの基本のコーディング

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+// tailwind.config.js
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+      colors: {
+      },
+      extend: {
+      },
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+  }
